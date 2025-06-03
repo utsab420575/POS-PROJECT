e8ea49f496f7aaab1fb466d594898f3942c8176e
utsab420575 <utsab@duet.ac.bd>
2025-06-03 21:26:08 +0600

    1. Employee Management Part 1

---
+++ app/Models/Employee.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+    //this is for fillable all column in Employee Table
+    protected $guarded = [];
+
+    //or,protected $fillable = [
+    //        'name',
+    //        'email',
+    //        -------
+    //        ----
+    //    ];
+}
