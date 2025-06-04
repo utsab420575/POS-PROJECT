e1ee634d41d60f2826c6e6f3bb2eccfedb8e0a3a
utsab420575 <utsab@duet.ac.bd>
2025-06-04 12:03:13 +0600

    3. Customer Management Part 3

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -79,4 +79,111 @@ class CustomerController extends Controller
         return redirect()->route('all.customer')->with($notification);
     } // End Method
 
+
+    public function EditCustomer($id){
+
+        $customer = Customer::findOrFail($id);
+        return view('backend.customer.edit_customer',compact('customer'));
+
+    } // End Method
+
+
+    public function UpdateCustomer(Request $request){
+
+        $customer_id = $request->id;
+
+        if ($request->file('image')) {
+
+            $recive_image = $request->file('image');
+            $name_gen = hexdec(uniqid()).'.'.$recive_image->getClientOriginalExtension();
+
+            // create image manager with desired driver
+            $manager = new ImageManager(new Driver());
+
+            // read image from file system
+            $image = $manager->read($recive_image);
+
+            // resize image proportionally to 300px width
+            //$image->scale(636,852);
+            //resize image without proportionally
+            $image->resize(300,300);
+
+            $path = public_path('upload/customer/');
+            $image->save($path . $name_gen);
+
+            $save_url = 'upload/customer/'.$name_gen;
+
+            Customer::findOrFail($customer_id)->update([
+
+                'name' => $request->name,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'address' => $request->address,
+                'shopname' => $request->shopname,
+                'account_holder' => $request->account_holder,
+                'account_number' => $request->account_number,
+                'bank_name' => $request->bank_name,
+                'bank_branch' => $request->bank_branch,
+                'city' => $request->city,
+                'image' => $save_url,
+                'created_at' => Carbon::now(),
+
+            ]);
+
+            $notification = array(
+                'message' => 'Customer Updated Successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->route('all.customer')->with($notification);
+
+        } else{
+
+            Customer::findOrFail($customer_id)->update([
+
+                'name' => $request->name,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'address' => $request->address,
+                'shopname' => $request->shopname,
+                'account_holder' => $request->account_holder,
+                'account_number' => $request->account_number,
+                'bank_name' => $request->bank_name,
+                'bank_branch' => $request->bank_branch,
+                'city' => $request->city,
+                'created_at' => Carbon::now(),
+
+            ]);
+
+            $notification = array(
+                'message' => 'Customer Updated Successfully',
+                'alert-type' => 'success'
+            );
+
+            return redirect()->route('all.customer')->with($notification);
+
+        } // End else Condition
+
+
+    } // End Method
+
+
+    public function DeleteCustomer($id){
+
+        $customer_img = Customer::findOrFail($id);
+        $img = $customer_img->image;
+        unlink($img);
+
+        Customer::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Customer Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+
+    } // End Method
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,9 @@ Route::middleware('auth')->group(function () {
         Route::get('/all/customer','AllCustomer')->name('all.customer');
         Route::get('/add/customer','AddCustomer')->name('add.customer');
         Route::post('/store/customer','StoreCustomer')->name('customer.store');
+        Route::get('/edit/customer/{id}','EditCustomer')->name('edit.customer');
+        Route::post('/update/customer','UpdateCustomer')->name('customer.update');
+        Route::get('/delete/customer/{id}','DeleteCustomer')->name('delete.customer');
     });
 });
 require __DIR__.'/auth.php';
