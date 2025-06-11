71f21575a8b4e1ab793a43359e6363dd6eb2d9d8
utsab420575 <utsab@duet.ac.bd>
2025-06-11 13:42:03 +0600

    1. How to Create 404 Page

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('index')->middleware('permission:employee.all');
+    return view('index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
